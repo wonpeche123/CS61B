6ef9e3c509758bc6ae6dad6c30b8910ccac33336
Wonpeche <wonpeche@gmail.com>
2024-10-15 21:00:30 +0800

    New library

---
+++ lab1/SLList.java
@@ -0,0 +1,31 @@
+public class SLList {
+    private static class IntNode{
+        public int item;
+        public IntNode next;
+
+        public IntNode (int first , IntNode node ){
+            item = first;
+            next = node;
+        }
+    }
+
+    private IntNode first;
+
+    public SLList(int x) {
+        first = new IntNode(x, null);
+    }
+
+    /** Adds an item to the front of the list. */
+    public void addFirst(int x) {
+        first = new IntNode(x, first);
+    }
+
+    public void addLast(int x) {
+        ;
+    }
+
+    public int size(){
+        int len = 0;
+
+    }
+}
