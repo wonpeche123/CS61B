97a10a9ba63deb801d45397842b941cfa1e01aa6
Wonpeche <wonpeche@gmail.com>
2024-11-12 14:51:13 +0800

    init

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -2,7 +2,14 @@ package gitlet;
 
 // TODO: any imports you need here
 
+import java.io.File;
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date; // TODO: You'll likely use this in this class
+import java.util.Locale;
+import static gitlet.Utils.*;
 
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
@@ -10,7 +17,7 @@ import java.util.Date; // TODO: You'll likely use this in this class
  *
  *  @author TODO
  */
-public class Commit {
+public class Commit implements Serializable {
     /**
      * TODO: add instance variables here.
      *
@@ -21,6 +28,75 @@ public class Commit {
 
     /** The message of this Commit. */
     private String message;
+    /** The timestamp of this Commit. */
+    private String timestamp;
+    /** The Unix time. */
+    private Date time;
+    /** The parent_1 of this Commit. */
+    private Commit parent_1;
+    /** The parent_2 of this Commit. */
+    private Commit parent_2;
+    /** The blobs of this Commit. */
+    private ArrayList<Node> blobs;
+    /** The hashcode of this Commit. */
+    private String id;
+
+    public static final File CWD = new File(System.getProperty("user.dir"));
+    /** The .gitlet directory. */
+    public static final File GITLET_DIR = join(CWD, ".gitlet");
+
+
+    /** A Node is a blob. */
+    private class Node {
+        public String filename;
+        public String sha1Hash;
+
+        public Node(String filename ,String hashcode) {
+            this.filename = filename;
+            this.sha1Hash = hashcode;
+        }
+    }
+
 
     /* TODO: fill in the rest of this class. */
+
+    public Commit() {
+
+        message = "initial commit";
+        time = new Date(0);
+        timestamp = timeToTimeStamp(time);
+        parent_1 = null;
+        parent_2 = null;
+        blobs = new ArrayList<>();
+//  只针对初始化时没有parents指针的情况，这种特殊的对应也能保证hash的区分度
+        id = sha1(message, timestamp, "null", "null", blobs.toString());
+    }
+
+    public Commit(String message, Commit parent_1, Commit parent_2, ArrayList<Node> blobs) {
+        this.message = message;
+        time = new Date(0);
+        timestamp = timeToTimeStamp(time);
+        this.parent_1 = parent_1;
+        this.parent_2 = parent_2;
+        this.blobs = blobs;
+        id = sha1(message, timestamp, parent_1.toString(), parent_2.toString(), blobs.toString());
+    }
+
+    /** 将UNIX时间转化成特定的时间戳格式*/
+    private static String timeToTimeStamp(Date time) {
+        DateFormat dateFormat = new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy Z", Locale.US);
+        return dateFormat.format(time);
+    }
+    /** Commit Methods*/
+
+    /** 获得当前Commit的hashcode*/
+    public String getid() {
+        return id;
+    }
+
+    public void save() {
+        File COMMIT_FILE = join(GITLET_DIR, "object", id);
+        writeObject(COMMIT_FILE, this);
+    }
+
 }
--- proj2/gitlet/Repository.java
+++ proj2/gitlet/Repository.java
@@ -1,6 +1,8 @@
 package gitlet;
 
 import java.io.File;
+import java.io.IOException;
+
 import static gitlet.Utils.*;
 
 // TODO: any imports you need here
@@ -21,9 +23,71 @@ public class Repository {
      */
 
     /** The current working directory. */
+    /*
+     *   .gitlet
+     *      |--objects
+     *      |     |--commit and blob
+     *      |--refs
+     *      |    |--heads
+     *      |         |--master
+     *      |--HEAD
+     *      |--stage
+     */
     public static final File CWD = new File(System.getProperty("user.dir"));
     /** The .gitlet directory. */
     public static final File GITLET_DIR = join(CWD, ".gitlet");
+    /** The refs directory. */
+    public static final File REFS_DIR = join(GITLET_DIR, "refs");
+    /** The heads directory. */
+    public static final File HEADS_DIR = join(REFS_DIR, "heads");
+    /** The object directory. */
+    public static final File OBJECT_DIR = join(GITLET_DIR, "object");
+    /** 当前的HEAD指针指向的Commit */
+    public static final File HEAD = join(GITLET_DIR, "HEAD");
+    /** 暂存区 */
+    public static final File STAGE = join(GITLET_DIR, "stage");
 
-    /* TODO: fill in the rest of this class. */
+    public static Commit currCommit;
+
+    /** init */
+    public void init () {
+        if (GITLET_DIR.exists()) {
+            System.out.println("A Gitlet version-control system already exists in the current directory.");
+            System.exit(0);
+        }
+        GITLET_DIR.mkdir();
+        OBJECT_DIR.mkdir();
+        REFS_DIR.mkdir();
+        HEADS_DIR.mkdir();
+        initCommit();
+        initHeads();
+        initHEAD();
+
+    }
+
+    private void initCommit() {
+        Commit initial_commit  = new Commit();
+        currCommit = initial_commit;
+        initial_commit.save();
+    }
+
+    private void initHeads() {
+        File headOfBranch = join(HEADS_DIR, "master");
+        writeObject(headOfBranch, currCommit.getid());
+    }
+
+    private void initHEAD() {
+        writeObject(HEAD, HEADS_DIR.toString() + "/master");
+    }
+
+
+    public void add(File filename) {
+        if (!filename.exists()) {
+            System.out.println("File does not exist.");
+            return;
+        }
+        String content = readContentsAsString(filename);
+        String sha1hash = sha1(content);
+
+    }
 }
